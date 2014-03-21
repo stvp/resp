4fe69e1ec2e49d9ef1226618f771f1afffef7a59
Tyson Tate <tyson@tysontate.com>
2014-03-21 16:28:54 -0700

    Add tests for multiple Read calls.

--- reader_test.go
+++ reader_test.go
@@ -2,6 +2,7 @@ package resp
 
 import (
 	"bytes"
+	"io"
 	"reflect"
 	"testing"
 )
@@ -64,6 +65,86 @@ func TestReadObjectSlice_Valid(t *testing.T) {
 		if err != nil {
 			t.Errorf("tests[%d]: %s", i, err.Error())
 		} else if !reflect.DeepEqual(test.expected, object) {
+			t.Errorf("tests[%d]:\nexpected: %v\ngot: %v", i, test.expected, object)
+		}
+	}
+}
+
+func TestReadObjectSlice_BufferErrors(t *testing.T) {
+	reply := []byte("-OK\r\n")
+	reader := NewReaderSize(bytes.NewReader(reply), len(reply)-1)
+	object, err := reader.ReadObjectSlice()
+	if err != ErrBufferFull {
+		t.Errorf("expected ErrBufferFull but got %#v", err)
+	}
+	if !reflect.DeepEqual(object, reply[0:len(reply)-1]) {
+		t.Errorf("expected: %v\ngot: %v", reply[0:len(reply)-1], object)
+	}
+}
+
+type multipleReadTest struct {
+	reads    [][]byte
+	expected []byte
+}
+
+func TestReadObjectSlice_MultipleReads_Valid(t *testing.T) {
+	tests := []multipleReadTest{
+		{
+			[][]byte{[]byte("-O"), []byte("K\r"), []byte("\n")},
+			[]byte("-OK\r\n"),
+		},
+		{
+			[][]byte{[]byte("$3\r"), []byte("\nfo"), []byte("o\r\n")},
+			[]byte("$3\r\nfoo\r\n"),
+		},
+		{
+			[][]byte{[]byte("*2\r\n"), []byte("-OK\r"), []byte("\n-O"), []byte("K\r\n")},
+			[]byte("*2\r\n-OK\r\n-OK\r\n"),
+		},
+		{
+			[][]byte{[]byte("*2\r\n*"), []byte("1\r\n-OK\r"), []byte("\n-O"), []byte("K\r\n")},
+			[]byte("*2\r\n*1\r\n-OK\r\n-OK\r\n"),
+		},
+	}
+
+	for i, test := range tests {
+		readers := []io.Reader{}
+		for _, piece := range test.reads {
+			readers = append(readers, bytes.NewReader(piece))
+		}
+		reader := NewReader(io.MultiReader(readers...))
+		object, err := reader.ReadObjectSlice()
+		if err != nil {
+			t.Errorf("tests[%d]: %s", i, err.Error())
+		} else if !reflect.DeepEqual(test.expected, object) {
+			t.Errorf("tests[%d]:\nexpected: %v\ngot: %v", i, test.expected, object)
+		}
+	}
+}
+
+func TestReadObjectSlice_MultipleReads_Invalid(t *testing.T) {
+	tests := []multipleReadTest{
+		{
+			[][]byte{[]byte("-O"), []byte("K\r")},
+			[]byte("-OK\r"),
+		},
+		{
+			[][]byte{[]byte("$3\r")},
+			[]byte("$3\r"),
+		},
+	}
+
+	for i, test := range tests {
+		readers := []io.Reader{}
+		for _, piece := range test.reads {
+			readers = append(readers, bytes.NewReader(piece))
+		}
+		reader := NewReader(io.MultiReader(readers...))
+		object, err := reader.ReadObjectSlice()
+		if err == nil {
+			t.Errorf("tests[%d]: expected error but didn't get one", i)
+		}
+		if !reflect.DeepEqual(test.expected, object) {
 			t.Errorf("tests[%d]:\nexpected: %v\ngot: %v", i, test.expected, object)
 		}
 	}
